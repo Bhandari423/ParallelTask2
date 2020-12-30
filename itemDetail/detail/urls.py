a346e4039045ed26c3de4f4ec1ca63e21379eaf7
shubhangi <shubhangi.bhandari@iaugmentor.com>
2020-12-30 17:45:01 +0530

    task2

--- itemDetail/detail/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("create-item/", views.CreateDetail.as_view()),
-    path("show-item/", views.CreateDetail.as_view()),
-    path("delete-item/<int:pk>", views.CreateDetail.as_view()),
-]
